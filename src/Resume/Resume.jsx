0d99c19424b823c52f2481136c9d71db2d102658
KindOfKind <stivir12@gmail.com>
2023-07-05 15:34:53 +0300

    Added 3 links on resume page

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -11,9 +11,15 @@ export default function Resume(props) {
             <img className={styles.background} src="PortfResumeBG1.png" alt="Фон" />
 
             <div className={styles.youtubeVideoContainer}>
-               <a href="https://youtube.com/playlist?list=PLENdZm7P-ZaiqD7HuvIX104i4cop8HgNH" target="_blank">Альбом со всеми видео.</a>
+               <a href="https://youtube.com/playlist?list=PLENdZm7P-ZaiqD7HuvIX104i4cop8HgNH" target="_blank">Альбом со всеми видео с главной страницы.</a>
                <br/>
                <a href="https://youtu.be/J1ihiuToFOU" target="_blank">Развлекательный видеоролик по игре PAS.</a>
+               <br/>
+               <a href="neonpawn.itch.io/phantom-art-sequence?password=Neon_ITMO" target="_blank">PAS на Itch.io.</a>
+               <br/>
+               <a href="https://www.youtube.com/watch?v=9-voy3AOLRM" target="_blank">Трейлер игры "Hell Over Me".</a>
+               <br/>
+               <a href="https://spazrael.itch.io/hell-over-me" target="_blank">"Hell Over Me" на Itch.io.</a>
             </div>
         </div>
     );
