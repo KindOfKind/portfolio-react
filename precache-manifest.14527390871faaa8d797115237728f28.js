ae94d556b6d4bc60a0032d64d289a8b515f02640
KindOfKind <stivir12@gmail.com>
2023-12-25 03:44:52 +0300

    Updates

---
+++ precache-manifest.14527390871faaa8d797115237728f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faa01bacc890546c6d4",
+    "url": "/portfolio-react/static/css/main.a60bf035.chunk.css"
+  },
+  {
+    "revision": "1faa01bacc890546c6d4",
+    "url": "/portfolio-react/static/js/main.cfcbb812.chunk.js"
+  },
+  {
+    "revision": "7f310946cf79ad85e774",
+    "url": "/portfolio-react/static/js/runtime~main.a8072891.js"
+  },
+  {
+    "revision": "9639da4b52d62a08f70b",
+    "url": "/portfolio-react/static/js/2.baa207d6.chunk.js"
+  },
+  {
+    "revision": "edfea3dbdadc740c7615",
+    "url": "/portfolio-react/static/js/3.3a0c354c.chunk.js"
+  },
+  {
+    "revision": "22eb4ac7dd79003f74e632fa78444411",
+    "url": "/portfolio-react/static/media/PortfBG.22eb4ac7.png"
+  },
+  {
+    "revision": "1fdfd98dae86a9eb258aebb13d3d212f",
+    "url": "/portfolio-react/index.html"
+  }
+];
